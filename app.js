56e1ddadde2e3c0becf993bcf5c0042de323c2ee
Hack Reactor Student <student@hackreactor.com>
2017-06-02 16:42:43 -0700

    Add list bold function

--- app.js
+++ app.js
@@ -1,14 +1,94 @@
-var GroceryListItem = (props) => (
-  <div>
-    <h2>Grocery List</h2>
-      <ul> 
-        <li>{props.grocery[0]} </li>
-        <li>{props.grocery[1]} </li>
-      </ul>
-  </div>
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // `state` is just an object literal
+    this.state = {
+      done: false
+    };
+  }
+
+  // When a list item is clicked, we will toggle the `done`
+  // boolean, and our component's `render` method will run again
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    // Making the style conditional on our `state` lets us 
+    // update it based on user interactions.
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    // You can pass inline styles using React's `style` attribute to any component
+    // snake-cased css properties become camelCased this this object
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.GroceryItems}</li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.todos.map(GroceryItems =>
+      <GroceryListItem key={GroceryItems} GroceryItems={GroceryItems} />
+    )}
+  </ul>
 );
 
-ReactDOM.render(
-  <GroceryListItem grocery={['Cucumbers', 'Kale']} />,
-  document.getElementById('app')
-);
+
+ReactDOM.render(<GroceryList todos = {['cucumber', 'kale']}/>, document.getElementById("app"));
+
+
+
+
+
+
+// class TodoListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     // `state` is just an object literal
+//     this.state = {
+//       done: false
+//     };
+//   }
+
+//   // When a list item is clicked, we will toggle the `done`
+//   // boolean, and our component's `render` method will run again
+//   onListItemClick() {
+//     this.setState({
+//       done: !this.state.done
+//     });
+//   }
+
+//   render() {
+//     // Making the style conditional on our `state` lets us 
+//     // update it based on user interactions.
+//     var style = {
+//       textDecoration: this.state.done ? 'line-through' : 'none'
+//     };
+
+//     // You can pass inline styles using React's `style` attribute to any component
+//     // snake-cased css properties become camelCased this this object
+//     return (
+//       <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+//     );
+//   }
+// }
+
+
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(todo =>
+//       <TodoListItem key={todo} todo={todo} />
+//     )}
+//   </ul>
+// );
+
+
+// ReactDOM.render(<TodoList todos = {['cucumber', 'kale']}/>, document.getElementById("app"));
